5a858080551ec5c2662b5d3898f3aa9fb0b1ee32
rskarz <riskazuliyantiiiii@gmail.com>
2023-03-29 19:27:51 +0700

    pertemuan ke 3

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -31,32 +31,33 @@ else
            $this->load->view('buku/daftarbuku',$data);
            $this->load->view('templates/templates-user/footer',$data);
   }
+  $this->load->view('templates/templates-user/modal');
    }
    public function detailBuku()
-   {
-       $id = $this->uri->segment(3);
-       $buku = $this->ModelBuku->joinKategoriBuku(['buku.id' => $id])->result();
+    {
+        $id = $this->uri->segment(3);
+        $buku = $this->ModelBuku->joinKategoriBuku(['buku.id' => $id])->result();
 
-       $data['user'] = "Pengunjung";
-       $data['title'] = "Detail Buku";
+        $data['user'] = "Pengunjung";
+        $data['title'] = "Detail Buku";
 
-       foreach ($buku as $fields) {
-           $data['judul'] = $fields->judul_buku;
-           $data['pengarang'] = $fields->pengarang;
-           $data['penerbit'] = $fields->penerbit;
-           $data['kategori'] = $fields->kategori;
-           $data['tahun'] = $fields->tahun_terbit;
-           $data['isbn'] = $fields->isbn;
-           $data['gambar'] = $fields->image;
-           $data['dipinjam'] = $fields->dipinjam;
-           $data['dibooking'] = $fields->dibooking;
-           $data['stok'] = $fields->stok;
-           $data['id'] = $id;
-       }
+        foreach ($buku as $fields) {
+            $data['judul'] = $fields->judul_buku;
+            $data['pengarang'] = $fields->pengarang;
+            $data['penerbit'] = $fields->penerbit;
+            $data['kategori'] = $fields->kategori;
+            $data['tahun'] = $fields->tahun_terbit;
+            $data['isbn'] = $fields->isbn;
+            $data['gambar'] = $fields->image;
+            $data['dipinjam'] = $fields->dipinjam;
+            $data['dibooking'] = $fields->dibooking;
+            $data['stok'] = $fields->stok;
+            $data['id'] = $id;
+        }
 
-       $this->load->view('templates/templates-user/header', $data);
-       $this->load->view('buku/detail-buku', $data);
-       $this->load->view('templates/templates-user/footer');
-   }
-
+        $this->load->view('templates/templates-user/header', $data);
+        $this->load->view('buku/detail-buku', $data);
+        $this->load->view('templates/templates-user/footer');
+        $this->load->view('templates/templates-user/modal');
+    }
 }
